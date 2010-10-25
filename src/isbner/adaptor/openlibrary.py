ddc8812036aed8a7b0cbe3d51d464873961b10df
Oleg Smirnov <oleg.smirnov@gmail.com>
2010-10-25 19:13:28 +0800

    OpenLibrary adaptor.

---
+++ src/isbner/adaptor/openlibrary.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from adaptor import Adaptor
+
+class OpenLibrary(Adaptor):
+    def __init__(self):
+        self._name = "OpenLibrary"
+        self._weight = 50
+
+    def _run(self, isbn):
+        try:
+            from django.utils import simplejson
+        except ImportError
+            import simplejson
+
+        url = 'http://openlibrary.org/api/books?bibkeys=ISBN:%s&jscmd=data&format=json' % (isbn)
+        json = simplejson.loads(self.fetch(url))
+
+        result = dict()
+        try:
+            json = json['ISBN:'+isbn]
+            result['title'] = json['title']
+            result['author'] = ', '.join([i['name'] for i in json['authors']])
+            result['isbn'] = isbn
+            result['publisher'] = ', '.join([i['name'] for i in json['publishers']])
+            result['date'] = json['publish_date']
+            result['source'] = json['url']
+        finally:            
+            return result
+
+    def check(self):
+        return self._run('0201558025') == {
+            'title': u'Concrete mathematics',
+            'author': u'Ronald L. Graham, Donald Knuth, Oren Patashnik',
+            'isbn': u'0201558025',
+            'publisher': u'Addison-Wesley',
+            'date': u'1994',
+            'source': u'http://openlibrary.org/books/OL1429049M/Concrete_mathematics'}
