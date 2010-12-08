83e54d162b77740fe78697cd4dc6fd6ae64757f2
Oleg Smirnov <oleg.smirnov@gmail.com>
2010-12-08 20:45:24 +0200

    Status backend.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 
 import logging
 import os
-from google.appengine.api.labs import taskqueue
+from google.appengine.api import taskqueue
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
@@ -30,6 +30,7 @@ class GetHandler(webapp.RequestHandler):
         self.response.out.write(simplejson.dumps(data))
 
 class ViewHandler(webapp.RequestHandler):
+    # TODO: Looks horrible enough to be refactored
     def get(self):
         isbn = isbner.utils.sanitize(self.request.get('isbn'))
         template_values = {'book': {'isbn': isbn}}
@@ -61,23 +62,47 @@ class ViewHandler(webapp.RequestHandler):
             template_values['info'] = [{'key': k, 'value': data['fields'][k][0]} for k in keys]
         self.response.out.write(template.render(path, template_values))
 
+class ProvidersHandler(webapp.RequestHandler):
+    def get(self):
+        template_values = {'status': memcache.get('status')}
+        path = os.path.join(os.path.dirname(__file__), 'static', 'providers.html')
+        self.response.out.write(template.render(path, template_values))
+
+class StatusHandler(webapp.RequestHandler):
+    def get(self):
+        for worker in isbner.names:
+            taskqueue.add(url='/worker/%s' % worker, params={'check': '1'})
+        self.response.set_status(200)
+
 def workers_factory():
     for (name, adaptor_class) in zip(isbner.names, isbner.classes):
         class AdaptorWorker(webapp.RequestHandler):
-            def post(self):
-                isbn = isbner.utils.sanitize(self.request.get('isbn'))
+            def check(self):
+                data = memcache.get('status') or dict()
+                data[self.adaptor.name] = self.adaptor.check()
+                memcache.set('status', data, time=86400)
+
+            def run(self, isbn):
+                isbn = isbner.utils.sanitize(isbn)
                 data = self.adaptor.dump(isbn)                
                 cached = memcache.get(isbn, namespace='isbn')
                 if cached is not None:
                     data = isbner.utils.merge(cached, data)
                 memcache.set(isbn, data, time=86400, namespace='isbn')
+
+            def post(self):
+                if self.request.get('check'):
+                    self.check()
+                else:
+                    self.run(self.request.get('isbn'))
                 self.response.set_status(200)
+
             adaptor = adaptor_class()
         yield((name, AdaptorWorker))
 
 def statics_factory():
-    urls = ['/', '/terms/', '/api/', '/providers/']
-    filenames = ['index.html', 'terms.html', 'api.html', 'providers.html']
+    urls = ['/', '/terms/', '/api/']
+    filenames = ['index.html', 'terms.html', 'api.html']
     for (url, filename) in zip(urls, filenames):
         class StaticHandler(webapp.RequestHandler):
             def get(self):
@@ -88,7 +113,9 @@ def statics_factory():
 
 def main():
     urls = [('/get/?', GetHandler),
-            ('/view/?', ViewHandler)]
+            ('/view/?', ViewHandler),
+            ('/status/', StatusHandler),
+            ('/providers/', ProvidersHandler)]
     for (url, worker) in statics_factory():
         urls.append((url, worker))
     for (name, worker) in workers_factory():
